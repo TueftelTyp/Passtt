b0c10bf835d99441762cf2ebb862cd377aa4c41e
Tüftel Typ <31390221+TueftelTyp@users.noreply.github.com>
2025-02-10 09:24:41 +0100

    SourceCode

---
+++ SourceCode/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace PassGen
+{
+    partial class PassForm
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PassForm));
+            this.chkUppercase = new System.Windows.Forms.CheckBox();
+            this.chkLowercase = new System.Windows.Forms.CheckBox();
+            this.chkNumbers = new System.Windows.Forms.CheckBox();
+            this.chkSpecialChars = new System.Windows.Forms.CheckBox();
+            this.chkSpaces = new System.Windows.Forms.CheckBox();
+            this.nudPasswordCount = new System.Windows.Forms.NumericUpDown();
+            this.nudPasswordLength = new System.Windows.Forms.NumericUpDown();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.lstPasswords = new System.Windows.Forms.ListBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.urlLabel = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPasswordCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPasswordLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkUppercase
+            // 
+            this.chkUppercase.AutoSize = true;
+            this.chkUppercase.Location = new System.Drawing.Point(113, 17);
+            this.chkUppercase.Name = "chkUppercase";
+            this.chkUppercase.Size = new System.Drawing.Size(47, 17);
+            this.chkUppercase.TabIndex = 2;
+            this.chkUppercase.Text = "ABC";
+            this.chkUppercase.UseVisualStyleBackColor = true;
+            // 
+            // chkLowercase
+            // 
+            this.chkLowercase.AutoSize = true;
+            this.chkLowercase.Location = new System.Drawing.Point(166, 17);
+            this.chkLowercase.Name = "chkLowercase";
+            this.chkLowercase.Size = new System.Drawing.Size(44, 17);
+            this.chkLowercase.TabIndex = 3;
+            this.chkLowercase.Text = "abc";
+            this.chkLowercase.UseVisualStyleBackColor = true;
+            // 
+            // chkNumbers
+            // 
+            this.chkNumbers.AutoSize = true;
+            this.chkNumbers.Location = new System.Drawing.Point(216, 18);
+            this.chkNumbers.Name = "chkNumbers";
+            this.chkNumbers.Size = new System.Drawing.Size(44, 17);
+            this.chkNumbers.TabIndex = 4;
+            this.chkNumbers.Text = "123";
+            this.chkNumbers.UseVisualStyleBackColor = true;
+            // 
+            // chkSpecialChars
+            // 
+            this.chkSpecialChars.AutoSize = true;
+            this.chkSpecialChars.Location = new System.Drawing.Point(266, 18);
+            this.chkSpecialChars.Name = "chkSpecialChars";
+            this.chkSpecialChars.Size = new System.Drawing.Size(45, 17);
+            this.chkSpecialChars.TabIndex = 5;
+            this.chkSpecialChars.Text = "#=+";
+            this.chkSpecialChars.UseVisualStyleBackColor = true;
+            // 
+            // chkSpaces
+            // 
+            this.chkSpaces.AutoSize = true;
+            this.chkSpaces.Location = new System.Drawing.Point(316, 18);
+            this.chkSpaces.Name = "chkSpaces";
+            this.chkSpaces.Size = new System.Drawing.Size(55, 17);
+            this.chkSpaces.TabIndex = 6;
+            this.chkSpaces.Text = "space";
+            this.chkSpaces.UseVisualStyleBackColor = true;
+            // 
+            // nudPasswordCount
+            // 
+            this.nudPasswordCount.Location = new System.Drawing.Point(56, 15);
+            this.nudPasswordCount.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudPasswordCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudPasswordCount.Name = "nudPasswordCount";
+            this.nudPasswordCount.Size = new System.Drawing.Size(39, 20);
+            this.nudPasswordCount.TabIndex = 1;
+            this.nudPasswordCount.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudPasswordLength
+            // 
+            this.nudPasswordLength.Location = new System.Drawing.Point(56, 45);
+            this.nudPasswordLength.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.nudPasswordLength.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudPasswordLength.Name = "nudPasswordLength";
+            this.nudPasswordLength.Size = new System.Drawing.Size(39, 20);
+            this.nudPasswordLength.TabIndex = 7;
+            this.nudPasswordLength.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(7, 47);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(43, 13);
+            this.lblLength.TabIndex = 8;
+            this.lblLength.Text = "Length:";
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(7, 17);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(46, 13);
+            this.lblAmount.TabIndex = 9;
+            this.lblAmount.Text = "Amount:";
+            // 
+            // lstPasswords
+            // 
+            this.lstPasswords.Font = new System.Drawing.Font("Candara", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPasswords.FormattingEnabled = true;
+            this.lstPasswords.ItemHeight = 23;
+            this.lstPasswords.Location = new System.Drawing.Point(10, 85);
+            this.lstPasswords.Name = "lstPasswords";
+            this.lstPasswords.Size = new System.Drawing.Size(361, 303);
+            this.lstPasswords.TabIndex = 12;
+            this.lstPasswords.TabStop = false;
+            this.toolTip1.SetToolTip(this.lstPasswords, "Copied !");
+            this.lstPasswords.Click += new System.EventHandler(this.lstPasswords_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(113, 45);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(258, 23);
+            this.btnGenerate.TabIndex = 8;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Copied!";
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.urlLabel.Font = new System.Drawing.Font("Consolas", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.urlLabel.Location = new System.Drawing.Point(353, 0);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(29, 9);
+            this.urlLabel.TabIndex = 13;
+            this.urlLabel.TabStop = true;
+            this.urlLabel.Text = "GitHub";
+            this.urlLabel.Click += new System.EventHandler(this.urlLabel_Click);
+            // 
+            // PassForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 398);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lstPasswords);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.nudPasswordLength);
+            this.Controls.Add(this.nudPasswordCount);
+            this.Controls.Add(this.chkSpaces);
+            this.Controls.Add(this.chkSpecialChars);
+            this.Controls.Add(this.chkNumbers);
+            this.Controls.Add(this.chkLowercase);
+            this.Controls.Add(this.chkUppercase);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PassForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Passtt";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPasswordCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPasswordLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkUppercase;
+        private System.Windows.Forms.CheckBox chkLowercase;
+        private System.Windows.Forms.CheckBox chkNumbers;
+        private System.Windows.Forms.CheckBox chkSpecialChars;
+        private System.Windows.Forms.CheckBox chkSpaces;
+        private System.Windows.Forms.NumericUpDown nudPasswordCount;
+        private System.Windows.Forms.NumericUpDown nudPasswordLength;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.ListBox lstPasswords;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.LinkLabel urlLabel;
+    }
+}
+
---
+++ SourceCode/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace PassGen
+{
+    public partial class PassForm : Form
+    {
+        private Random random = new Random();
+
+        public PassForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lstPasswords.Items.Clear();
+            int passwordCount = (int)nudPasswordCount.Value;
+            int passwordLength = (int)nudPasswordLength.Value;
+
+            for (int i = 0; i < passwordCount; i++)
+            {
+                string password = GeneratePassword(passwordLength);
+                lstPasswords.Items.Add(password);
+            }
+        }
+
+        private string GeneratePassword(int length)
+        {
+            string characterSet = "";
+            if (chkUppercase.Checked) characterSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            if (chkLowercase.Checked) characterSet += "abcdefghijklmnopqrstuvwxyz";
+            if (chkNumbers.Checked) characterSet += "0123456789";
+            if (chkSpecialChars.Checked) characterSet += "!@#$%^&*()_+-=[]{}|/;~:',.<>?";
+            if (chkSpaces.Checked) characterSet += " ";
+
+            if (string.IsNullOrEmpty(characterSet))
+            {
+                return string.Empty;
+            }
+
+            StringBuilder password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(characterSet.Length);
+                password.Append(characterSet[index]);
+            }
+
+            return password.ToString();
+        }
+
+        private void lstPasswords_Click(object sender, EventArgs e)
+        {
+            if (lstPasswords.SelectedItem != null) {
+                string selectedItem = lstPasswords.SelectedItem.ToString();
+                Clipboard.SetText(selectedItem);
+                toolTip1.SetToolTip(lstPasswords, lstPasswords.SelectedItem + "");
+            }
+        }
+
+        private void urlLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/TueftelTyp/Passtt");
+        }
+    }
+}
